786252081eb90483e588d2eaf9082874b364ce33
Suryarao Bethapudi <15906282+shiningsurya@users.noreply.github.com>
2020-02-11 17:43:01 -0700

    Fixed indentation

--- src/Header.hpp
+++ src/Header.hpp
@@ -4,20 +4,20 @@
 // One Header to rule them all
 
 struct Header {
- // station
- int stationid;
- // strings
- char utc_start_str[64];
- char name[16];
- char sigproc_file[256];
- // positions
- double ra, dec;
- // frequency
- double fch1, foff, cfreq, bandwidth;
- // time
- double tsamp, tstart, epoch;
- // memory
- int nbits, nchans, nifs, npol;
+  // station
+  int stationid;
+  // strings
+  char utc_start_str[64];
+  char name[16];
+  char sigproc_file[256];
+  // positions
+  double ra, dec;
+  // frequency
+  double fch1, foff, cfreq, bandwidth;
+  // time
+  double tsamp, tstart, epoch;
+  // memory
+  int nbits, nchans, nifs, npol;
 };
 
 typedef struct Header Header_t;
