234a99ca70fb8f6de2cda46d0647bf8b339429a1
Suryarao Bethapudi <15906282+shiningsurya@users.noreply.github.com>
2020-02-28 11:33:37 -0700

    print structs

--- src/Header.hpp
+++ src/Header.hpp
@@ -56,3 +56,59 @@ typedef struct {
   char name[16];
   char sigproc_file[64];
 } trigHead_t;
+
+// packed trigger header struct
+// for trigDADA ease
+typedef struct {
+  // trigger stuff
+  double i0;
+  double i1;
+  float sn;
+  float dm;
+  float width;
+  float peak_time;
+  // header stuff
+  // station
+  int stationid;
+  // positions
+  double ra, dec;
+  // frequency
+  double fch1, foff;
+  // time
+  double tsamp, tstart, epoch;
+  // memory
+  unsigned nbits, nchans;
+} packth_t;
+
+// cout
+std::ostream& operator<< (std::ostream& os, const Header_t& h) {
+  // positions
+  std::cout << "RA     " << h.ra  << std::endl;
+  std::cout << "DEC    " << h.dec << std::endl;
+  // frequency
+  std::cout << "FCH1   " << h.fch1 << std::endl;
+  std::cout << "FOFF   " << h.foff << std::endl;
+  std::cout << "CFREQ  " << h.cfreq << std::endl;
+  std::cout << "BANDW  " << h.bandwidth<< std::endl;
+  // time
+  std::cout << "TSAMP  " << h.tsamp << std::endl;
+  std::cout << "TSTART " << h.tstart << std::endl;
+  std::cout << "EPOCH  " << h.epoch << std::endl;
+  // memory
+  std::cout << "NCHANS " << h.nchans<< std::endl;
+  std::cout << "NBITS  " << h.nbits << std::endl;
+  std::cout << "NPOL   " << h.npol << std::endl;
+  // strings
+  std::cout << "UTC    " << h.utc_start_str << std::endl;
+  std::cout << "SOURCE " << h.name << std::endl;
+  std::cout << "SIGFIL " << h.sigproc_file << std::endl;
+  std::cout << "STATID " << h.stationid << std::endl;
+}
+std::ostream& operator<< (std::ostream& os, const trigger_t& t) {
+  std::cout << "I0     " << t.i0 << std::endl;
+  std::cout << "I1     " << t.i1 << std::endl;
+  std::cout << "SN     " << t.sn << std::endl;
+  std::cout << "DM     " << t.dm << std::endl;
+  std::cout << "WD     " << t.width*1e3 << std::endl;
+  std::cout << "PT     " << t.peak_time << std::endl;
+}
